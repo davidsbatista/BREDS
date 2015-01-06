fabd16e1ed09c0f64cc61df56677cdfea33298fc
davidsbatista <dsbatista@gmail.com>
2015-01-06 18:18:57 +0000

    fixed bug generating Terms

--- build-ground-truth/multiprocessing-query.py
+++ build-ground-truth/multiprocessing-query.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import re
 
 __author__ = 'dsbatista'
 
-from multiprocessing.dummy import Process
+from whoosh.analysis import StopFilter, RegexTokenizer
 import multiprocessing
 import fileinput
 import functools
@@ -28,22 +29,55 @@ def timecall(f):
 
 
 def multi_thread_query(relationships, results, e1_type, e2_type):
-    idx = open_dir("index")
+    idx = open_dir("index_test")
+    regex_tokenize = re.compile('\w+|-|<[A-Z]+>[^<]+</[A-Z]+>', re.U)
+    tokenizer = RegexTokenizer(regex_tokenize)
+    stopper = StopFilter()
+
     with idx.searcher() as searcher:
         while True:
             r = relationships.get_nowait()
             # calculate the PMI
             entity1 = "<"+e1_type+">"+r.ent1+"</"+e1_type+">"
             entity2 = "<"+e2_type+">"+r.ent2+"</"+e2_type+">"
+
+            terms = list()
+            for token in stopper(tokenizer(unicode(r.between), renumber=True)):
+                terms.append(query.Term("sentence", token.text))
+
+            #print terms
             t1 = query.Term("sentence", entity1)
-            t2 = query.Term("sentence", r.between)  # TODO: remover stopwords do r.between
+            #t2 = query.Term("sentence", r.between)  # TODO: remover stopwords do r.between
             t3 = query.Term("sentence", entity2)
-            q1 = spans.SpanNear2([t1, t2, t3], slop=1, ordered=True)
-            q2 = spans.SpanNear2([t1, t3], slop=1, ordered=True)
+
+            query_terms = list()
+            query_terms.append(t1)
+            for t in terms:
+                query_terms.append(t)
+            query_terms.append(t3)
+
+            q1 = spans.SpanNear2(query_terms, slop=2, ordered=True)
+            q2 = spans.SpanNear2([t1, t3], slop=8, ordered=True)
             entities_r = searcher.search(q1)
             entities = searcher.search(q2)
 
-            print entity1, '\t', r.between, '\t', entity2, len(entities_r), len(entities)
+            """
+            print query_terms, len(entities_r)
+            print [t1, t3], len(entities)
+            print "\n"
+            """
+
+            #print entity1, '\t', r.between, '\t', entity2, len(entities_r), len(entities)
+
+            try:
+                assert not len(entities_r) > len(entities)
+            except AssertionError, e:
+                print e
+                print r.sentence
+                print r.ent1
+                print r.ent2
+                print query_terms
+                print [t1, t3]
 
             if len(entities) > 0:
                 pmi = float(len(entities_r)) / float(len(entities))
@@ -71,7 +105,7 @@ def main():
     e1_type = "ORG"
     e2_type = "PER"
     #num_cpus = multiprocessing.cpu_count()
-    num_cpus = 5
+    num_cpus = 1
 
     processes = [multiprocessing.Process(target=multi_thread_query, args=(relationships, results, e1_type, e2_type, )) for i in range(num_cpus)]
     for proc in processes:
