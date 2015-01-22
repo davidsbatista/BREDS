df3d9a41caef00320c7f4b8b90f95054484ddd7f
davidsbatista <dsbatista@gmail.com>
2015-01-22 19:40:59 +0000

    fixed a BUG in sentence selection

--- automatic-evaluation/select-sentences-freebase.py
+++ automatic-evaluation/select-sentences-freebase.py
@@ -39,7 +39,17 @@ def load_freebase_entities(directory):
 
 def load_sentences(data_file):
     sentences = manager.Queue()
+    count = 0
     with codecs.open(data_file, 'rb', encoding='utf-8') as f:
+        for line in f:
+            sentences.put(line.strip())
+            count += 1
+            if count % 100000 == 0:
+                print count, "processed"
+
+        return sentences
+
+        """
         # Size 0 will read the ENTIRE file into memory!
         # File is open read-only
         m = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
@@ -56,26 +66,33 @@ def load_sentences(data_file):
             if count % 100000 == 0:
                 print count, "processed"
         return sentences
+        """
 
 
 def get_sentences(sentences, freebase, results):
     count = 0
     while True:
         sentence = sentences.get_nowait()
+        discard = False
         count += 1
         s = Sentence(sentence.strip(), None, None, MAX_TOKENS_AWAY, MIN_TOKENS_AWAY, CONTEXT_WINDOW)
         for r in s.relationships:
             if r.between == " , " or r.between == " ( " or r.between == " ) ":
-                continue
+                discard = True
+                break
             else:
-                try:
-                    freebase[r.ent1]
-                    freebase[r.ent2]
-                except KeyError:
-                    continue
+                if r.ent1 not in freebase or r.ent2 not in freebase:
+                    discard = True
+                    break
+        if len(s.relationships) == 0:
+            discard = True
+        if discard is False:
             results.append(sentence)
+            print "added", sentence, len(sentence)
+
         if count % 50000 == 0:
             print multiprocessing.current_process(), "queue size", sentences.qsize()
+
         if sentences.empty is True:
             break
 
