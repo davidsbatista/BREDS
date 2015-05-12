fa777da48bc03dd696e1e56c858700b74289fd3a
davidsbatista <dsbatista@gmail.com>
2015-05-12 16:32:22 +0100

    added another example

--- Common/example_parse.py
+++ Common/example_parse.py
@@ -24,6 +24,7 @@ def main():
     examples = ["Amazon.com founder and chief executive Jeff Bezos."]
     examples = ["Google based in Mountain View, California."]
     examples = ["NBC is currently a poor third in the ratings of the four US networks , having slipped from first place when General Electric acquired it from RCA in 1985 ."]
+    examples = ["The new channel will focus on `` people , contemporary and historical , in and out of the headlines '' according to Westinghouse Electric , the company that acquired CBS last year for 5.4 billion dollars ."]
 
     for s in examples:
         print s
