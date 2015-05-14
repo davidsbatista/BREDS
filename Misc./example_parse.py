c158bbaf698ec4b8676dc7a2ea4e8c1119eb2357
davidsbatista <dsbatista@gmail.com>
2015-05-14 20:41:01 +0100

    added a few more examples: TODO: compute shortest dependency path

--- Misc./example_parse.py
+++ Misc./example_parse.py
@@ -21,14 +21,16 @@ def main():
                 "Hello, My name is Melroy.", "What is your name?",
                 "Amazon.com chief executive Jeff Bezos"]
     """
-    examples = ["Amazon.com founder and chief executive Jeff Bezos."]
-    examples = ["Google based in Mountain View, California."]
-    examples = ["NBC is currently a poor third in the ratings of the four US networks , having slipped from first place when General Electric acquired it from RCA in 1985 ."]
-    examples = ["The new channel will focus on `` people , contemporary and historical , in and out of the headlines '' according to Westinghouse Electric , the company that acquired CBS last year for 5.4 billion dollars ."]
+    #examples = ["Amazon.com founder and chief executive Jeff Bezos."]
+    #examples = ["Google based in Mountain View, California."]
+    #examples = ["NBC is currently a poor third in the ratings of the four US networks , having slipped from first place when General Electric acquired it from RCA in 1985 ."]
+    #examples = ["The new channel will focus on `` people , contemporary and historical , in and out of the headlines '' according to Westinghouse Electric , the company that acquired CBS last year for 5.4 billion dollars ."]
     #sentence: In favour of the deal were <PER>Ted Turner</PER> , founder and boss of <ORG>TBS</ORG> , a cable-based business , and <ORG>Time Warner</ORG> chairman <PER>Gerald Levin</PER> , whose empire already held an 18 percent stake in <ORG>TBS</ORG> .
     #sentence: Including the roughly 1,500 workers picked up in the <ORG>DoubleClick</ORG> acquisition , <ORG>Google</ORG> now has more than 18,000 employees worldwide .
-    examples = ["The story was first seen at Techcrunch , the picked up by the Wall Street Journal and has since been the subject of much talk , posts and thoughts over the past few days and finally it has been confirmed that <p1> Google </p1> have purchased <p2> Youtube </p2> for $ 1.65 billion in an official statement ."]
-    examples = ["For KFC , whose Louisville headquarters resemble a white-columned mansion , the recipe is more than a treasured link to its roots"]
+    #examples = ["The story was first seen at Techcrunch , the picked up by the Wall Street Journal and has since been the subject of much talk , posts and thoughts over the past few days and finally it has been confirmed that Google have purchased Youtube for $ 1.65 billion in an official statement ."]
+    #examples = ["For KFC , whose Louisville headquarters resemble a white-columned mansion , the recipe is more than a treasured link to its roots"]
+    examples = ["Dan was not born in Lisbon", "Dan was born in Lisbon"]
+    examples = ["Lucent , based in Murray Hill , filed the lawsuit in federal court in Newark this month ."]
     for s in examples:
         print s
         t = parser.raw_parse(s)
