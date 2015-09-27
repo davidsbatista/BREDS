c387be9a54bfd32c171446742ca3a88a8ea3ed5e
davidsbatista <dsbatista@gmail.com>
2015-09-27 18:46:55 +0100

    updates index if already exists

--- automatic-evaluation/index-whoosh.py
+++ automatic-evaluation/index-whoosh.py
@@ -12,8 +12,15 @@ import codecs
 
 from whoosh.analysis import RegexTokenizer
 from whoosh.fields import Schema, TEXT
-from whoosh.index import create_in
+from whoosh.index import create_in, open_dir
 from whoosh.query import *
+
+from nltk.corpus import stopwords
+from Sentence import Sentence
+
+bad_tokens = [",", "(", ")", ";", "''",  "``", "'s", "-", "vs.", "v", "'", ":", ".", "--"]
+stopwords_list = stopwords.words('english')
+not_valid = bad_tokens + stopwords_list
 
 
 def timecall(f):
@@ -27,30 +34,46 @@ def timecall(f):
     return wrapper
 
 
-@timecall
 def create_index():
     regex_tokenize = re.compile('\w+(?:-\w+)+|<[A-Z]+>[^<]+</[A-Z]+>|\w+', re.U)
     tokenizer = RegexTokenizer(regex_tokenize)
     schema = Schema(sentence=TEXT(stored=True, analyzer=tokenizer))
     if not os.path.exists("index_full"):
         os.mkdir("index_full")
-    idx = create_in("index_full", schema)
+        idx = create_in("index_full", schema)
+    else:
+        idx = open_dir("index_full")
     return idx
-
 
 @timecall
 def index_sentences(writer):
-    #total: 26.729.482
     count = 0
     f = codecs.open(sys.argv[1], "r", "utf-8")
+    max_tokens = 6
+    min_tokens = 1
+    context_window = 2
     for l in f:
-        try:
-            writer.add_document(sentence=l.strip())
-        except UnicodeDecodeError, e:
-            print e
-            print l
-            sys.exit(0)
+        valid = 0
+        invalid = 0
+        s = Sentence(l, max_tokens, min_tokens, context_window)
+        if len(s.relationships) == 0:
+            continue
+        else:
+            for r in s.relationships:
+                if "." in r.between:
+                    invalid += 1
+                elif all(x in not_valid for x in r.between):
+                    invalid += 1
+                else:
+                    valid += 1
 
+            if valid > invalid:
+                try:
+                    writer.add_document(sentence=l.strip())
+                except UnicodeDecodeError, e:
+                    print e
+                    print l
+                    sys.exit(0)
         count += 1
         if count % 50000 == 0:
             print count, "lines processed"
