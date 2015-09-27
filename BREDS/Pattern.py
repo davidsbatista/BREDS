15e4dab8d9ef58b7241acc5ea1e4ae8fdee113c5
davidsbatista <dsbatista@gmail.com>
2015-09-27 18:47:16 +0100

    removed uneeded stuff

--- BREDS/Pattern.py
+++ BREDS/Pattern.py
@@ -5,33 +5,26 @@ __author__ = "David S. Batista"
 __email__ = "dsbatista@inesc-id.pt"
 
 import uuid
-from numpy import zeros
 from math import log
 
 
 class Pattern(object):
 
-    def __init__(self, config, t=None):
+    def __init__(self, t=None):
         self.id = uuid.uuid4()
-        self.single_vector = zeros(config.vec_dim)
         self.positive = 0
         self.negative = 0
         self.unknown = 0
         self.confidence = 0
         self.tuples = set()
-        self.vectors = list()
-        self.patterns_words = set()
         if tuple is not None:
             self.tuples.add(t)
 
     def __hash__(self):
-        return hash((self.patterns_words, self.tuples))
+        return hash(self.tuples)
 
     def __eq__(self, other):
-        return (self.tuples, self.patterns_words) == (other.tuples, other.patterns_words)
-
-    def __str__(self):
-        return " | ".join([p for p in self.patterns_words]).encode("utf8")
+        return self.tuples == other.tuples
 
     def __cmp__(self, other):
         if other.confidence > self.confidence:
@@ -51,11 +44,6 @@ class Pattern(object):
 
     def add_tuple(self, t):
         self.tuples.add(t)
-
-    def merge_patterns(self):
-        for t in self.tuples:
-            for p in t.patterns_words:
-                self.patterns_words.add(p)
 
     def update_selectivity(self, t, config):
         for s in config.positive_seed_tuples:
